b7e9c7dc2e0b14708a6637fc445f6201afa62faa
ABirjandi <a.birjandi@mahware.com>
2026-07-23 03:47:21 +0330

    feat(assets): add new SVG icons and images for improved UI elements

--- src/components/shared/header.component.js
+++ src/components/shared/header.component.js
@@ -10,11 +10,13 @@ export const HeaderComponent = {
             <i class="fa-regular fa-bars"></i>
           </button>
 
-          <div class="w-10 h-10 lg:hidden flex flex-row justify-center items-center">
+          <div
+            class="w-10 h-10 lg:hidden flex flex-row justify-center items-center"
+          >
             <a href="/">
               <img
                 id="logo"
-                src="/public/picture/logo.png"
+                src="../../../picture/logo.png"
                 class="logo h-full justify-center shadow-brand/10"
                 title="Habit Tracker"
                 alt="Habit Tracker Logo"
